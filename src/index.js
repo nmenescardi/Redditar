e5ffedf45217616f8e425c5abd92153c9e26d5c8
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-06-28 09:54:05 -0300

    Add sass structure and basic styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Redditar from './Redditar';
+import './Redditar.scss';
 
 ReactDOM.render(<Redditar />, document.getElementById('root'));
