0b4ed7d67da65ee25bb4c0233d2e1782bfbf1b45
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2022-06-30 21:50:09 -0300

    Add types to sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -4,19 +4,24 @@ import CardList from '../card/CardList';
 import DismissAll from '../extra/DismissAll';
 import { connect } from 'react-redux';
 import { isSidebarOpen } from '../../store/posts/selector';
+import { Store } from '../../types';
 
-const Sidebar = ({ sidebarOpen }) => {
+type Props = {
+  sidebarOpen: boolean;
+};
+
+const Sidebar: React.FC<Props> = ({ sidebarOpen }) => {
   return (
     <section className={`sidebar ${sidebarOpen ? 'sidebar--open' : null} `}>
       <Navbar />
       <CardList />
 
-      <DismissAll className="sidebar__dimiss-all" />
+      <DismissAll className="sidebar__dismiss-all" />
     </section>
   );
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state: Store) => ({
   sidebarOpen: isSidebarOpen(state),
 });
 export default connect(mapStateToProps)(Sidebar);
