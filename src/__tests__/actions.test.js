03bc62694d0f1c204fb8ae8d3223b3256ebd0a80
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2022-07-18 09:35:34 -0300

    add fixtures - test card component

--- src/__tests__/actions.test.js
+++ src/__tests__/actions.test.js
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 import { expect } from 'chai';
 import {
   postSetData,
---
+++ src/__tests__/card.test.js
@@ -0,0 +1,49 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import postsJson from './fixtures/posts.json';
+import Card from '../components/card/Card';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import Store from '../store';
+import { postSetData } from '../store/posts/actions';
+import fromNow from '../utils/fromNow';
+
+describe('Single Card', () => {
+  const posts = postsJson.data.children;
+
+  const store = Store();
+  store.store.dispatch(postSetData(posts));
+
+  const firstPost = posts[0].data;
+
+  it('should render its props', () => {
+    const card = render(
+      <Provider store={store.store}>
+        <Card postId={firstPost.id}></Card>
+      </Provider>
+    );
+
+    const comments = card.container.querySelector('.card__comments');
+    expect(comments).toHaveTextContent('958 Comments');
+
+    const timeAgo = card.container.querySelector('.card__time');
+    expect(timeAgo).toHaveTextContent(fromNow(firstPost.created));
+
+    const author = card.container.querySelector('.card__author');
+    expect(author).toHaveTextContent(firstPost.author);
+
+    const thumbnail = card.container.querySelector('.card__thumbnail img');
+    expect(thumbnail).toHaveAttribute('src', firstPost.thumbnail);
+
+    const title = card.container.querySelector('.card__title');
+    expect(title).toHaveTextContent(firstPost.title);
+  });
+
+  it.todo('Check onClick behavior: card shown visited');
+  it.todo('Check onClick behavior: post details visible');
+  it.todo('Check onClick behavior: toggle layout on mobile');
+
+  it.todo('Check onClick Dismiss button');
+});
